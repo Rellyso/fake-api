fc72fbca75a9c2e20bafd63a9200a63f14eac172
Rellyson Douglas <rellysondouglas2015@gmail.com>
2024-06-20 13:27:22 -0300

    fix: adjust server to work and make post request

--- src/server.ts
+++ src/server.ts
@@ -8,38 +8,25 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, '..', 'db.json'))
 const middlewares = jsonServer.defaults()
 
-
+server.use(middlewares)
 server.use(jsonServer.bodyParser);
 
+server.post('/query-status', (req: Request, res: Response) => {
+  const { cpf, phoneNumber } = req.body;
+  const dbPath = path.join(__dirname, '..', 'db.json');
+  const dbJson = fs.readFileSync(dbPath, 'utf8');
+  const db = JSON.parse(dbJson);
 
-// Middleware para adicionar atrasos nas respostas (opcional)
-server.use(middlewares)
+  const client = db.clients.find((client: any) => client.cpf === cpf);
 
-server.use((req, res, next) => {
-  setTimeout(() => next(), 1300)
-})
-
-server.use((req, res, next) => {
-  if (req.method === "POST" || req.method === "PUT") {
-    const { cpf, phoneNumber } = req.body;
-    const dbPath = path.join(__dirname, '..', 'db.json');
-    const dbJson = fs.readFileSync(dbPath, 'utf8');
-    const db = JSON.parse(dbJson);
-
-    const client = db.clients.find((client: any) => client.cpf === cpf);
-
-    if (client && client.blocked) {
-      return res.status(400).json({ error: "Houve um problema com o CPF consultado e por isso não podemos seguir" });
-    }
-
-    if (!client || client.phoneNumber !== phoneNumber) {
-      return res.status(400).json({ error: "Existe outro número de celular associado a este CPF e por isso não é possível realizar a consulta. Verifique com o cliente o celular cadastrado anteriormente para consultar o crédito disponível" });
-    }
+  if (client && client.blocked) {
+    return res.status(400).json({ message: "Houve um problema com o CPF consultado e por isso não podemos seguir" });
   }
-  next();
-});
 
-server.use(router);
+  if (!client || client.phoneNumber !== phoneNumber) {
+    return res.status(400).json({ message: "Existe outro número de celular associado a este CPF e por isso não é possível realizar a consulta. Verifique com o cliente o celular cadastrado anteriormente para consultar o crédito disponível" });
+  }
+});
 
 server.get('/query-status/count', (req: Request, res: Response) => {
   const db = router.db<QueryStatus> // Obter a instância do lowdb
@@ -81,6 +68,10 @@ server.get('/query-status/count', (req: Request, res: Response) => {
   }, { approvedConsult: 0, pending: 0, finished: 0, purchased: 0, total: 0 })
 
   res.json(counts)
+})
+
+server.use((req, res, next) => {
+  setTimeout(() => next(), 1300)
 })
 
 server.use(router)
